822785b6e51625dcdb2c7d80727334dd92075e2c
Yogesh <yogeshkumar.05@live.in>
2018-11-15 10:40:29 +0530

    section card

--- src/components/About.js
+++ src/components/About.js
@@ -9,6 +9,14 @@ const About = (props) => {
             <ul>
                 <li>Born and raised in <a href='https://en.wikipedia.org/wiki/Hassan,_Karnataka' target='_blank'>Hassan</a>, Karnakata</li>
                 <li>Residing in <a href='https://en.wikipedia.org/wiki/Bangalore' target='_blank'>Bangalore</a>, Karnakata</li> 
+                <li>Studied till +2 in Jawahar Navodaya Vidyalaya, Hassan</li>
+                <li>Graduated in 2012 from National Institute of Technology Karnataka in Information Technology</li>
+                <li>Open to learn new things</li>
+                <li>Staunch follower of Yoga</li>
+                <li>Likes to read Motivational books</li>
+                <li>Good at Swimming, Cricket, Volleyball, Table Tennis</li>
+                <li>Loves to train people on new Technologies</li>
+                <li>Knows four languages</li>
             </ul>
         </div>
     </div>);
--- src/components/Main.js
+++ src/components/Main.js
@@ -70,6 +70,39 @@ export default class Main extends Component {
         <li>15+ trainings</li>
     </ul>
     </div>
+    <div class="row">
+  <div class="column">
+    <div class="card">
+      <p><i class="fa fa-user"></i></p>
+      <h3>11+</h3>
+      <p>Partners</p>
+    </div>
+  </div>
+
+  <div class="column">
+    <div class="card">
+      <p><i class="fa fa-check"></i></p>
+      <h3>55+</h3>
+      <p>Projects</p>
+    </div>
+  </div>
+  
+  <div class="column">
+    <div class="card">
+      <p><i class="fa fa-smile-o"></i></p>
+      <h3>100+</h3>
+      <p>Happy Clients</p>
+    </div>
+  </div>
+  
+  <div class="column">
+    <div class="card">
+      <p><i class="fa fa-coffee"></i></p>
+      <h3>100+</h3>
+      <p>Meetings</p>
+    </div>
+  </div>
+</div>
     </div>);
 }
 }
